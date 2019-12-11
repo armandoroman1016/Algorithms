c7468cfb2732474722ecbe3af8a9578c9bdfd99b
Armando Roman <armandoroman1016@gmail.com>
2019-12-11 14:49:38 -0800

    recipe batches optomized, returns as soon as a zero appears

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -5,21 +5,34 @@ import math
 def recipe_batches(recipe, ingredients):
 
   # TODO possibly get each recipe key and divide by ingredients key, choose lowest number out of all possible equations
-  ingredient_amounts = []
+  # TODO if ingredient amounts gets a zero added return 0 immediately
+    # ingredient_amounts = []
+    count = 0
+    max_batches = 0
+    for key in recipe:
+        recipe_amount = recipe.get(key)
+        ingredient_amount = ingredients.get(key)
+        if count == 0:
+          max_batches = ingredient_amount // recipe_amount
+        else:
+            if max_batches == 0:
+                break
+                return
+            else:  
+                if ingredient_amount == None:
+                    max_batches = 0
+                    break
+                    return
+                elif max_batches > ingredient_amount // recipe_amount:
+                     max_batches = ingredient_amount // recipe_amount
 
-  for key in recipe:
-    if ingredients.get(key) == None:
-      ingredient_amounts.append(0)
-    else:
-      recipe_amount = recipe.get(key)
-      ingredient_amount = ingredients.get(key)
-      ingredient_amounts.append(ingredient_amount // recipe_amount)
+        count += 1
 
-  return min(ingredient_amounts)
+    return max_batches
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 98, 'butter': 100, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
