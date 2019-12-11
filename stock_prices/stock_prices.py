7d1028ab2bb14d2b350628c22fc091a4ccb15e94
Armando Roman <armandoroman1016@gmail.com>
2019-12-11 13:06:34 -0800

    stock prices complete

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,19 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # MP = set initial max profit to prices[1] - prices[0]
+  max_profit = prices[1] - prices[0]
+ 
+  # iterate over items and find difference between items to the right
+  for i in range(0, len(prices) - 1):
+    for j in range(0, i):
+      possible_profit = prices[i] - prices[j]
+  # if price[i] > price[j] compare that difference to current MP
+      if max_profit < possible_profit:
+  # if greater then MP update MP to that value
+        max_profit = possible_profit
 
+  return max_profit
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
@@ -13,3 +24,5 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
+  
