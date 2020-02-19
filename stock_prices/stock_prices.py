b57ed102acd7fd0482f116dcbc23ff0537c77077
Armando Roman <armandoroman1016@gmail.com>
2020-02-19 07:33:20 -0800

    stock prices passing tests

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,9 +1,32 @@
 #!/usr/bin/python
 
+# TODO try to eleminate double for loops with some sort of data structure
+
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # initialize max profit 
+
+  last_trade_idx = len(prices) - 1
+
+  max_profit = prices[last_trade_idx] - prices[last_trade_idx - 1]
+
+  # loop through the array compare all previous elements
+  for i in range(last_trade_idx, -1, -1):
+    for j in range(i - 1, -1, -1):
+    # if cur value - prev value is greater than max profit update that variable
+      potential = prices[i] - prices[j]
+
+      if potential > max_profit:
+        max_profit = potential
+
+  return max_profit
+
+  
+
+prices = [1050, 270, 1540, 3800, 2]
+
+print(find_max_profit(prices))
 
 
 if __name__ == '__main__':
