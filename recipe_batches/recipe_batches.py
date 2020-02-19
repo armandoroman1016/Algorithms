629794d357df159a127adcadb234cefd1baeecbd
Armando Roman <armandoroman1016@gmail.com>
2020-02-19 08:02:45 -0800

    recipe batches passing tests

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,26 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+
+  max_batches = []
+
+  for key in recipe:
+
+    stock_item = ingredients.get(key, None)
+
+    if stock_item is not None:
+      
+      stock_item = int(stock_item)
+      recipe_ing = int(recipe[f"{key}"])
+      batches_possible = stock_item // recipe_ing
+      max_batches.append(batches_possible)
+
+    else:
+      max_batches.append(0)
+      break
+
+  return min(max_batches)
+
 
 
 if __name__ == '__main__':
